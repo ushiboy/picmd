1ab4154a74e7aee5014620545e9f4e1eb187df4d
ushiboy <ushiboy.dev@gmail.com>
2020-05-06 10:20:22 +0900

    fix impl

--- picmd/_receiver.py
+++ picmd/_receiver.py
@@ -1,6 +1,5 @@
 from typing import Optional
 import struct
-import binascii
 from ._const import CMD_PREFIX, \
         CMD_PREFIX_LEN, \
         CMD_END, \
@@ -10,8 +9,7 @@ from ._const import CMD_PREFIX, \
         CMD_PARITY_LEN, \
         MAX_DATA_SIZE
 from ._data import CommandRequest
-from ._exception import InvalidFormatException, \
-        InvalidLengthException
+from ._exception import InvalidLengthException
 
 CMD_STATIC_SIZE = CMD_PREFIX_LEN + CMD_LEN + CMD_DATA_SIZE_LEN
 UNINITIALIZED_DATA_SIZE = MAX_DATA_SIZE + 1
@@ -54,17 +52,10 @@ class ATCommandReceiver:
         if self._cur_cmd_data_size == UNINITIALIZED_DATA_SIZE and \
                 self._buffered_size >= CMD_STATIC_SIZE:
             t = self._buffered[CMD_PREFIX_LEN + CMD_LEN: CMD_STATIC_SIZE]
-            try:
-                self._cur_cmd_data_size = struct.unpack('<H', binascii.a2b_hex(t))[0]
-            except binascii.Error:
-                # reset
-                self._ready_to_receive_command = False
-                self._buffered = self._buffered[CMD_STATIC_SIZE:]
-                self._buffered_size = len(self._buffered)
-                raise InvalidFormatException
+            self._cur_cmd_data_size = struct.unpack('<H', t)[0]
 
     def pull_received_command(self) -> Optional[CommandRequest]:
-        d_end = CMD_STATIC_SIZE + self._cur_cmd_data_size * 2
+        d_end = CMD_STATIC_SIZE + self._cur_cmd_data_size
         p_end = d_end + CMD_PARITY_LEN
         end = p_end + CMD_END_LEN
 
@@ -88,10 +79,7 @@ class ATCommandReceiver:
         c = buf[CMD_PREFIX_LEN: CMD_PREFIX_LEN + CMD_LEN]
         p = buf[d_end: p_end]
 
-        try:
-            cmd = int.from_bytes(binascii.a2b_hex(c), 'big')
-            data = binascii.a2b_hex(buf[CMD_STATIC_SIZE: d_end])
-            parity = int.from_bytes(binascii.a2b_hex(p), 'big')
-            return CommandRequest(cmd, size, data, parity)
-        except binascii.Error:
-            raise InvalidFormatException
+        cmd = int.from_bytes(c, 'big')
+        data = buf[CMD_STATIC_SIZE: d_end]
+        parity = int.from_bytes(p, 'big')
+        return CommandRequest(cmd, size, data, parity)
