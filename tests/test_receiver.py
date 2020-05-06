c0b7f87e8d0bebdba9cc42ef76225edc5482cedd
ushiboy <ushiboy.dev@gmail.com>
2020-05-06 10:20:14 +0900

    fix request protocol spec

--- tests/test_receiver.py
+++ tests/test_receiver.py
@@ -30,38 +30,24 @@ def test_store_buff():
     assert r.buffered_size == 7 # cut noise
 
     # append command
-    r.store_buff(b'01')
+    r.store_buff(b'\x01')
     assert r.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE
-    assert r.buffered_size == 9
+    assert r.buffered_size == 8
 
     # append data size low
-    r.store_buff(b'ff')
-    assert r.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE
-    assert r.buffered_size == 11
-
-    # append data size high
-    r.store_buff(b'00')
-    assert r.cur_cmd_data_size == 255
-    assert r.buffered_size == 13
-
-    # append data buffer
-    r.store_buff(b'0000')
-    assert r.cur_cmd_data_size == 255 # not change
-    assert r.buffered_size == 17
-
-def test_store_buff_when_invalid_data_size():
-    r = ATCommandReceiver()
-    assert r.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE
-    assert r.buffered_size == 0
-
-    r.store_buff(b'AT*PIC=01')
+    r.store_buff(b'\xff')
     assert r.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE
     assert r.buffered_size == 9
 
-    with pytest.raises(InvalidFormatException):
-        r.store_buff(b'ZZZZ')
-    assert r.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE
-    assert r.buffered_size == 0 # reset
+    # append data size high
+    r.store_buff(b'\x00')
+    assert r.cur_cmd_data_size == 255
+    assert r.buffered_size == 10
+
+    # append data buffer
+    r.store_buff(b'\x00\x00')
+    assert r.cur_cmd_data_size == 255 # not change
+    assert r.buffered_size == 12
 
 def test_pull_received_command():
     r1 = ATCommandReceiver()
@@ -69,10 +55,10 @@ def test_pull_received_command():
     assert r1.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE
     assert r1.buffered_size == 0
 
-    r1.store_buff(b'AT*PIC=01000001\r')
+    r1.store_buff(b'AT*PIC=\x01\x00\x00\x01\r')
     assert r1.pull_received_command() is None
     assert r1.cur_cmd_data_size == 0
-    assert r1.buffered_size == 16
+    assert r1.buffered_size == 12
 
     r1.store_buff(b'\n')
     c1 = r1.pull_received_command()
@@ -85,9 +71,9 @@ def test_pull_received_command():
 
 
     r2 = ATCommandReceiver()
-    r2.store_buff(b'AT*PIC=0101000202\r\nAT*PIC=')
+    r2.store_buff(b'AT*PIC=\x01\x01\x00\x02\x02\r\nAT*PIC=')
     assert r2.cur_cmd_data_size == 1
-    assert r2.buffered_size == 26
+    assert r2.buffered_size == 21
 
     c2 = r2.pull_received_command()
     assert c2.command == 0x01
@@ -99,35 +85,8 @@ def test_pull_received_command():
 
 
     r3 = ATCommandReceiver()
-    r3.store_buff(b'AT*PIC=0101000202\r?AT*PIC=')
+    r3.store_buff(b'AT*PIC=\x01\x01\x00\x02\x02\r?AT*PIC=')
     with pytest.raises(InvalidLengthException):
         r3.pull_received_command()
     assert r3.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE # reset
     assert r3.buffered_size == 7 # change
-
-def test_pull_received_command_when_invalid_command():
-    r1 = ATCommandReceiver()
-    r1.store_buff(b'AT*PIC=ZZ000001\r\n')
-
-    with pytest.raises(InvalidFormatException):
-        r1.pull_received_command()
-    assert r1.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE # reset
-    assert r1.buffered_size == 0 # change
-
-def test_pull_received_command_when_invalid_data():
-    r1 = ATCommandReceiver()
-    r1.store_buff(b'AT*PIC=010100ZZ01\r\n')
-
-    with pytest.raises(InvalidFormatException):
-        r1.pull_received_command()
-    assert r1.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE # reset
-    assert r1.buffered_size == 0 # change
-
-def test_pull_received_command_when_invalid_parity():
-    r1 = ATCommandReceiver()
-    r1.store_buff(b'AT*PIC=010000ZZ\r\n')
-
-    with pytest.raises(InvalidFormatException):
-        r1.pull_received_command()
-    assert r1.cur_cmd_data_size == UNINITIALIZED_DATA_SIZE # reset
-    assert r1.buffered_size == 0 # change
