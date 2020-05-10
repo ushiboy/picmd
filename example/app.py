2f6356a69e3790f8f2f5d38e6949ad4756fb7332
ushiboy <ushiboy.dev@gmail.com>
2020-05-10 13:14:28 +0900

    fix example app

--- example/app.py
+++ example/app.py
@@ -4,11 +4,11 @@ from picmd import PiCmd
 app = PiCmd.create('/dev/serial0')
 
 @app.handler(0x01)
-def greeting_handler(data: bytes, size: int) -> str:
+def greeting_handler() -> str:
     return 'hello world'
 
 @app.handler(0x02)
-def file_receive_handler(data: bytes, size: int):
+def file_receive_handler(data: bytes):
     with open('./tmp/received-%s.bin' % int(time.time()), 'wb') as f:
         f.write(data)
 
